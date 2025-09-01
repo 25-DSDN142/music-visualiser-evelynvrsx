e998b1ac2552f480cf61bf3dd9a1d5619aced057
Evelyn Veronica <165637656+evelynvrsx@users.noreply.github.com>
2025-09-02 11:37:38 +1200

    Base wave working

--- music_starter.js
+++ music_starter.js
@@ -1,11 +1,16 @@
 var col = 0;
 let numberOfFlowers = 5;
-let flowerDiameter = 100;
+let flowerDiameter = 70;
+let soundWaveRect = 30;
 let colors = [];
 let midFlowerX = 200;
 let midFlowerY = 300;
 let lerpAmount = 0;
 let lerpSpeed = 0.02;
+let angle = 0;
+let r = 100;
+let radius = 150;
+gap = 10;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 
@@ -14,8 +19,21 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+  let pink = color(230, 94, 225);
+  let yellow = color(254,214,123);
 
   randomSeed(3);
+
+  // Draw line for the sound wave
+  stroke(255);
+  strokeWeight(2);
+  lineXStart = width/5;
+  lineYPoint = height/2;
+  lineXEnd = 4*(width/5);
+  line(lineXStart, lineYPoint, lineXEnd, lineYPoint);
+
+  // Vocal parameters (not used as shifting starts anymore)
+  // We'll layout groups of 4 columns (vocal, drum, bass, other) next to each other
 
   // Update lerp amount for smooth color transition
   lerpAmount += lerpSpeed;
@@ -23,17 +41,59 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     lerpAmount = 0;
   }
   
-  let mappedVocal = map(vocal, 0, 100, 250, width-flowerDiameter/2);
-  draw_one_flower(mappedVocal, 100);
+  // Map volumes to bar heights
+  let maxBarHeight = height * 0.4;
+  let mappedVocal = map(vocal, 0, 100, 10, maxBarHeight);
+  let mappedDrum = map(drum, 0, 100, 10, maxBarHeight);
+  let mappedBass = map(bass, 0, 100, 10, maxBarHeight);
+  let mappedOther = map(other, 0, 100, 10, maxBarHeight);
 
- draw_multiple_flowers();
+  // compute how many groups of 4 columns fit between lineXStart and lineXEnd
+  let groupWidth = 4 * (soundWaveRect + gap);
+  let availableWidth = lineXEnd - lineXStart;
+  let maxGroups = floor(availableWidth / groupWidth);
+  let groups = min(17, maxGroups);
+
+  for (let i = 0; i < groups; i++) {
+    // base x for this group
+    let baseX = lineXStart + i * groupWidth + (soundWaveRect / 2);
+
+    let vocalX = baseX;
+    let drumX = baseX + (soundWaveRect + gap);
+    let bassX = baseX + 2 * (soundWaveRect + gap);
+    let otherX = baseX + 3 * (soundWaveRect + gap);
+
+    // draw each track in this group
+    // Vocal
+    fill(100, 100, 200);
+    rect(vocalX, lineYPoint, soundWaveRect, mappedVocal);
+
+    // Drum
+    fill(100, 200, 100);
+    rect(drumX, lineYPoint, soundWaveRect, mappedDrum);
+
+    // Bass
+    fill(200, 100, 100);
+    rect(bassX, lineYPoint, soundWaveRect, mappedBass);
+
+    // Other
+    fill(200, 200, 100);
+    rect(otherX, lineYPoint, soundWaveRect, mappedOther);
+  }
+
+  
+  //draw_one_flower(mappedDrum, 200);
+  //rect(mappedDrum, 200, flowerDiameter, flowerDiameter);
+
+  //draw_multiple_flowers();
+
+  
 }
 
 function draw_multiple_flowers() {
   for (let i = 1; i <= 5; i++) {
     let x = random(width);
     let y = random(height);
-
 
     draw_one_flower(x, y);
   }
@@ -48,7 +108,8 @@ function draw_one_flower(midFlowerX, midFlowerY) {
   let currentColor = lerpColor(pink, yellow, lerpAmount);
 
   let petalRotation = 360 / 5;
-  noFill();
+  //noFill();
+  fill(currentColor);
   
   for (let i = 0; i < 5; i++) {
     strokeWeight(3);
